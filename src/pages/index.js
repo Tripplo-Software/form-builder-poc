eeec88b26cca3d100902fe7fc1eb3fe0c3e1a8b4
AmoDinho <u14284783@tuks.co.za>
2021-03-16 22:01:25 +0200

    created components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import HeadingOne from "../components/HeadingOne";
+import FormBuilder from "../components/FormBuilder";
 const Index = () => {
+  const componets = [
+    {
+      component: <input />,
+    },
+    { component: <select /> },
+  ];
   return (
     <>
       <HeadingOne className="text-center text-blue-500">
