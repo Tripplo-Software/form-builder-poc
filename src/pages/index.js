b3ac1a3b8de0d2cef3522584500d770a2b65ab3c
AmoDinho <u14284783@tuks.co.za>
2021-03-17 22:09:13 +0200

    setting up hook form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import HeadingOne from "../components/HeadingOne";
 import FormBuilder from "../components/formBuilder";
+
 const Index = () => {
+  const { register, handleSubmit } = useForm();
   const [formData, setFormData] = useState({});
   const handleChange = (form) => setFormData(form);
   const components = [
     {
-      component: <input />,
+      component: <input ref={register} className="border border-red-400" />,
     },
-    { component: <button>children</button> },
+    { component: <input ref={register} className="border border-red-400" /> },
   ];
   console.log("formData", formData);
   return (
