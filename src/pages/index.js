2427a18c9b994e9f8d25dddea7822e0cef591254
AmoDinho <u14284783@tuks.co.za>
2021-04-22 23:21:18 +0200

    added stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,11 @@ import React, { useState } from "react";
 import HeadingOne from "../components/HeadingOne";
 import FormBuilder from "../components/formBuilder";
 import getItems from "./getItems";
-// const FancyInput = React.forwardRef((props, ref, onChange) => (
-//   <input
-//     ref={ref}
-//     className="border border-red-400"
-//     {...props}
-//     onChange={onChange}
-//   />
-// ));
+import getArrayItems from "./getArrayItems";
+
 const Index = () => {
   const [formData, setFormData] = useState({});
+  const [arrayData, setArrayData] = useState({});
   const handleChange = (data) => setFormData({ ...data });
   const ref = React.createRef();
 
@@ -21,8 +16,14 @@ const Index = () => {
       <HeadingOne className="text-center text-blue-500">
         This is the home page of the app!
       </HeadingOne>
-
+      <p>Single forms</p>
       <FormBuilder items={getItems()} data={formData} update={handleChange} />
+      <p>Array forms</p>
+      <FormBuilder
+        items={getArrayItems()}
+        data={formData}
+        update={handleChange}
+      />
 
       {/* <button onClick={() => handleSubmit(handleChange)}>Click me</button> */}
     </>
