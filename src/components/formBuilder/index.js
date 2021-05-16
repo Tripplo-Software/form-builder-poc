a51c272a814d8839d363d57e3b9fb71bf68c0a5e
AmoDinho <u14284783@tuks.co.za>
2021-05-16 21:21:36 +0200

    something is working

--- src/components/formBuilder/index.js
+++ src/components/formBuilder/index.js
@@ -66,7 +66,7 @@ const ArrayComponent = ({ item, update, data }) => {
           <div>
             {item.items.map((component) => (
               <component.component
-                onChange={(e, component) => mapComponentData(e, component)}
+                onChange={(e) => mapComponentData(e, component)}
               />
             ))}
 
